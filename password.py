3f4a24193d3cbb2bf22d56b17ac85fe5cd5f84f2
Thomas Lynch <tholynch@gmail.com>
2016-12-29 16:24:09 -0500

    added functioning password module

---
+++ password.py
@@ -0,0 +1,49 @@
+pws = ['about', 'after', 'again', 'below', 'could',
+       'every', 'first', 'found', 'great', 'house',
+       'large', 'learn', 'never', 'other', 'place',
+       'plant', 'point', 'right', 'small', 'sound',
+       'spell', 'still', 'study', 'their', 'there',
+       'these', 'thing', 'think', 'three', 'water',
+       'where', 'which', 'world', 'would', 'write']
+
+
+# check given options against list of passwords and return all possible
+#   solutions
+def solve(opts):
+    sols = pws.copy()   # get (deep) copy of password list
+
+    # for each possible password...
+    #   iterates over password list because copy gets modified by loop
+    for sol in pws:
+        # look at the columns one by one
+        for i, col in enumerate(opts):
+            # if this letter is not one of the options
+            if sol[i] not in col:
+                # check if removed already, and do it if not
+                if sol in sols: sols.remove(sol)
+
+    return sols # return solution list
+
+
+if __name__ == '__main__':
+    opts = []
+    solved = False
+    
+    i = 0
+    while not solved:
+        # get 'column'
+        n = input()
+        opts.append(list(n))
+        i += 1
+        
+        # check for solution with current data
+        s = solve(opts)
+        print(s)
+        if len(s) <= 1: solved = True
+
+    # print solution
+    if (s):
+        print('SOLUTION:', s[0])
+    else:
+        print('NO SOLUTION')
+
