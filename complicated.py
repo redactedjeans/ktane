f8471b82b1e8271328420b39544876bc6066ab3d
Thomas Lynch <tholynch@gmail.com>
2017-01-04 11:53:47 -0500

    added the complicated wire rules

--- complicated.py
+++ complicated.py
@@ -1,3 +1,44 @@
+# define all the rules as a 4-dimensional tuple
+# red,  blue,   star,   led
+# 0     0       0       0       C
+# 0     0       0       1       D
+# 0     0       1       0       C
+# 0     0       1       1       B
+# 0     1       0       0       S
+# 0     1       0       1       P
+# 0     1       1       0       D
+# 0     1       1       1       P
+# 1     0       0       0       S
+# 1     0       0       1       B
+# 1     0       1       0       C
+# 1     0       1       1       B
+# 1     1       0       0       S
+# 1     1       0       1       S
+# 1     1       1       0       P
+# 1     1       1       1       D
+rules = (
+         (
+          (
+           ('c', 'd'),
+           ('c', 'b')
+          ),
+          (
+           ('s', 'p'),
+           ('d', 'p')
+          )
+         ),
+         (
+          (
+           ('s', 'b'),
+           ('c', 'b')
+          ),
+          (
+           ('s', 's'),
+           ('p', 'd')
+          )
+         )
+        )
+
 def solve():
     pass
 
@@ -6,5 +47,7 @@ def solve_all():
 
 
 if __name__ == '__main__':
-    s = solve_all()
-    print(s)
+    print(rules[0][1][1][0].upper())    #D
+
+    # s = solve_all()
+    # print(s)
