1aa2722602193285338103a2f3cc89279c389c6e
Alejandro Lynch <alynch@hopscotch.ca>
2017-01-04 20:43:30 -0500

    beginning to navigate through maze

--- maze.py
+++ maze.py
@@ -44,7 +44,38 @@ maze = [
 
 
 def solve():
-    pass
+    print(maze)
+
+    curr_cell = 2
+    row = curr_cell // 6
+    col = curr_cell % 6
+
+    print(maze[curr_cell])
+
+    print(row, col)
+
+    open_wall = get_valid_cell(maze[curr_cell])
+
+    print(open_wall)
+
+    if open_wall == 0:
+        row -= 1
+
+    if open_wall == 1:
+        col += 1
+
+    if open_wall == 2:
+        row += 1
+
+    if open_wall == 3:
+        col -= 1
+
+    print(row, col)
+
+def get_valid_cell(cell):
+    for i, val in enumerate(cell):
+        if val == 0:
+            return i
 
 def solve_all():
     solve()
@@ -52,4 +83,3 @@ def solve_all():
 
 if __name__ == '__main__':
     s = solve_all()
-    print(s)
